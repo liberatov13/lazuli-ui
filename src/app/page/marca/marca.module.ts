ae5393e1dd77887ceb1428f63d28e4c24783f7e0
Elvis Liberato <liberatov13@gmail.com>
2024-02-27 01:31:54 -0300

    feat: criar formulário de marca
    LBT-25: Implementar uma tela que permita o cadastro de novas marcas

--- src/app/page/marca/marca.module.ts
+++ src/app/page/marca/marca.module.ts
@@ -5,18 +5,30 @@ import { MarcaListComponent } from './marca-list/marca-list.component';
 import { TableModule } from "primeng/table";
 import { MessagesModule } from "primeng/messages";
 import { TooltipModule } from "primeng/tooltip";
+import { MarcaFormComponent } from './marca-form/marca-form.component';
+import { FormsModule } from "@angular/forms";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
+import { ToggleButtonModule } from "primeng/togglebutton";
+import { DynamicDialogModule } from "primeng/dynamicdialog";
 
 
 @NgModule({
   declarations: [
     MarcaPageComponent,
-    MarcaListComponent
+    MarcaListComponent,
+    MarcaFormComponent
   ],
   imports: [
     CommonModule,
+    ButtonModule,
+    DynamicDialogModule,
+    FormsModule,
+    InputTextModule,
     MessagesModule,
     TableModule,
     TooltipModule,
+    ToggleButtonModule,
   ]
 })
 export class MarcaModule { }
